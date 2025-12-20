caef268af83e22afe8a4e32391c3bdeb0b5bd417
LUIS AMANCIO <amancio.ern@gmail.com>
2025-12-19 20:10:03 -0400

    IA10-MODF

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef } from "react";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
