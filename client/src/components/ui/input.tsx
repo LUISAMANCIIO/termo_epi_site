e3f20232745214e07f80677317cfe9245bd1635d
LUIS AMANCIO <amancio.ern@gmail.com>
2025-12-19 23:41:50 -0400

    IA19-MODF

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -1,5 +1,5 @@
 import { useDialogComposition } from "./dialog";
-import { useComposition } from "@/hooks/useComposition";
+import { useComposition } from "../../hooks/useComposition";
 import { cn } from "@/lib/utils";
 import * as React from "react";
 
